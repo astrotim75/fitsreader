a8db73dd836240dc3f9c1abec65b10a34d8b2349
Tim <astrotim75@gmail.com>
2026-08-31 12:20:02 +0100

    Stop tracking build directory

--- build/Desktop_Qt_6_11_1_MinGW_64_bit_Debug/CMakeFiles/FitsReader.dir/FitsReader_autogen/EWIEGA46WW/qrc_resources.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/FitsReader.dir/FitsReader_autogen/EWIEGA46WW/qrc_resources.cpp.obj: \
- C:\Users\timho\Documents\Programming\ Repo\Astro\FitsReader\build\Desktop_Qt_6_11_1_MinGW_64_bit_Debug\FitsReader_autogen\EWIEGA46WW\qrc_resources.cpp
